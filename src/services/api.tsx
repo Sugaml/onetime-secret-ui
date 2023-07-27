1116329633d88a114e49433ebbe4472d8ad32279
babulalt <babulal.tamang@berrybytes.com>
2023-07-27 22:38:49 +0545

    API endpoint implemented

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,24 +1,49 @@
-import axios from "axios";
+// api.ts
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
+// Define the ApiResponse interface
 interface ApiResponse {
   url: string;
 }
 
-export const generateSecretUrl = async (content: string,paraphase: string,days: number): Promise<string> => {
+// axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
+let baseURL = ''; // Initialize with a default value
+
+if (typeof process !== 'undefined' && process.env.API_BASE_URL) {
+  baseURL = process.env.API_BASE_URL;
+  console.log("baseurl ::",baseURL)
+}
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL,
+  // Other Axios configuration options
+});
+
+export default axiosInstance;
+
+export const generateSecretUrl = async (
+  content: string,
+  paraphrase: string,
+  days: number
+): Promise<string> => {
   try {
-    if (content.trim() === "") {
-      throw new Error("Content field cannot be empty.");
+    if (content.trim() === '') {
+      throw new Error('Content field cannot be empty.');
     }
 
-    const response = await axios.post<ApiResponse>("/api/generate-url", {
-      content,
-      paraphase,
-      days
-    });
+    const response: AxiosResponse<ApiResponse> = await axiosInstance.post<ApiResponse>(
+      '/one-time-secret',
+      {
+        content,
+        paraphrase,
+        days,
+      }
+    );
 
     return response.data.url;
   } catch (error) {
-    console.error("Error fetching the URL:", error);
+    console.error('Error fetching the URL:', error);
     throw error;
   }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig,loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.API_BASE_URL': JSON.stringify(env.API_BASE_URL)
   },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-})
+    plugins: [
+      react()
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 3000,
+    },
+  };
+});
